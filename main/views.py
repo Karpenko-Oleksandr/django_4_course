ac26b761e2a6bc0473d24b0ba4bdca76d23e6d06
Oleksandr Karpenko <1ooksapp8@gmail.com>
2025-01-01 12:10:15 +0200

    add urls to main app

--- main/views.py
+++ main/views.py
@@ -4,10 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     content = {
-        'title': 'HoneyShop',
-        'content': 'Main page - HoneyShop'
+        'title': 'Home - Головна',
+        'content': 'Furniture store HOME'
     }
     return render(request, 'main/index.html', content)
 
 def about(request):
-    return HttpResponse('About page')
+    content = {
+        'title': 'Home - Про нас',
+        'content': 'Про нас',
+        'test_on_page': "Магазин який допоможе тобі створити затишний дім."
+    }
+    return render(request, 'main/about.html', content)
