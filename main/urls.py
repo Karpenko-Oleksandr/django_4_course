3150ecdbfd50fe9ff10675e7973060f70089bdfe
Oleksandr Karpenko <1ooksapp8@gmail.com>
2025-01-01 12:41:17 +0200

    added goods app, connected catalog and product html

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from main import views as main_views
+from main import views
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', main_views.index, name='index'),
-    path('about/', main_views.about, name='about'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
 
