220d9459e9f7a50392cbef14903a018d7fe4f2d4
Oleksandr Karpenko <1ooksapp8@gmail.com>
2025-01-08 10:01:38 +0200

    Simple search created

--- app/settings.py
+++ app/settings.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
